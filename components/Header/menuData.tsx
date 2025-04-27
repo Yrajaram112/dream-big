901be96963fdb6993e0472783bd343b54d8af3b2
Rajaram Yadav <yrajaram112@gmail.com>
2025-04-27 14:43:54 -0500

    Dreaming Big 1st Step

---
+++ components/Header/menuData.tsx
@@ -0,0 +1,66 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "HOME",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "SCHOLARSHIPS",
+    path: "/scholarships",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "UNIVERSITIES",
+    path: "/universities",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "ABOUT US",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "SERVICES",
+    path: "/services",
+    newTab: false,
+  },
+  {
+    id: 5,
+    title: "CONTACT",
+    path: "/contact",
+    newTab: false,
+  },
+  // {
+  //   id: 6,
+  //   title: "Pages",
+  //   newTab: false,
+  //   submenu: [
+  //     {
+  //       id: 41,
+  //       title: "About Page",
+  //       path: "/about",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 42,
+  //       title: "Contact",
+  //       path: "/contact",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 43,
+  //       title: "Blog Grid Page",
+  //       path: "/blog",
+  //       newTab: false,
+  //     }
+  //   ],
+  // },
+];
+export default menuData;
